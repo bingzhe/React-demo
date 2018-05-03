555940dd380ae506c37a303bb77fee6801e6e07d
bingzhe <874177815@qq.com>
2018-05-03 18:59:55 +0800

    change

--- react-and-redux03/src/index.js
+++ react-and-redux03/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ControlPanel from './views/ControlPanel';
+import Provider from './Provider'
+import store from './Store.js';
+
 import './index.css';
 
 ReactDOM.render(
-  <ControlPanel />,
+  <Provider store={store}>
+    <ControlPanel />
+  </Provider>,
   document.getElementById('root')
 );
